7c081b2067a9f07b8c3d37337a42d0e8a49a6620
ganisaac <isaacganiyu01@gmail.com>
2024-01-16 11:57:23 +0100

    Update option_view.py

--- src/view/option_view.py
+++ src/view/option_view.py
@@ -118,12 +118,12 @@ class OptionView(AbstractView):
             # QUIT
             pass
 
-    def plot_greeks(self, S_min, S_max) :
+    def plot_greeks_S(self, S_min, S_max) :
         answers = prompt(self._questions)
         option= Option(answers["option_name"],float(answers["S0"]),float(answers["K"]),float(answers["T"]),float(answers["r"]))
         bsm = BS_formula(option, self.person)
 
-        def greeks(S):
+        def greeks_S(S):
 
             d1 = (np.log(S / option.K) + (option.r + 0.5 * bsm._sigma**2) * option.T) / (bsm._sigma * np.sqrt(option.T))
             d2 = d1 - bsm._sigma * np.sqrt(option.T)
@@ -138,16 +138,16 @@ class OptionView(AbstractView):
             vega = S * norm.pdf(d1) * np.sqrt(option.T)
 
             # Theta
-            theta = -(S * norm.pdf(d1) * sigma) / (2 * np.sqrt(T)) - r * K * np.exp(-r * T) * norm.cdf(d2)
+            theta = -(S * norm.pdf(d1) * bsm._sigma) / (2 * np.sqrt(option.T)) - option.r * option.K * np.exp(-option.r * option.T) * norm.cdf(d2)
 
             # Rho
-            rho = K * T * np.exp(-r * T) * norm.cdf(d2)
+            rho = option.K * option.T * np.exp(-option.r * option.T) * norm.cdf(d2)
 
             return delta, gamma, vega, theta, rho
 
         underlying_prices = np.linspace(S_min, S_max, 100)
 
-        greeks_values = np.array([greeks(price) for price in underlying_prices])
+        greeks_values = np.array([greeks_S(price) for price in underlying_prices])
 
         # Plot des grecques
         plt.figure(figsize=(12, 8))
@@ -159,6 +159,52 @@ class OptionView(AbstractView):
             plt.plot(underlying_prices, greeks_values[:, i], label=labels[i])
             plt.title(labels[i])
             plt.xlabel('Prix de l\'actif sous-jacent')
+            plt.legend()
+
+        plt.tight_layout()
+        plt.show()
+
+    def plot_greeks_K(self, K_min, K_max) :
+        answers = prompt(self._questions)
+        option= Option(answers["option_name"],float(answers["S0"]),float(answers["K"]),float(answers["T"]),float(answers["r"]))
+        bsm = BS_formula(option, self.person)
+
+        def greeks_K(K):
+
+            d1 = (np.log(option.S0 / K) + (option.r + 0.5 * bsm._sigma**2) * option.T) / (bsm._sigma * np.sqrt(option.T))
+            d2 = d1 - bsm._sigma * np.sqrt(option.T)
+
+            # Delta
+            delta = norm.cdf(d1)
+
+            # Gamma
+            gamma = norm.pdf(d1) / (option.S0 * bsm._sigma * np.sqrt(option.T))
+
+            # Vega
+            vega = option.S0 * norm.pdf(d1) * np.sqrt(option.T)
+
+            # Theta
+            theta = -(option.S0 * norm.pdf(d1) * bsm._sigma) / (2 * np.sqrt(option.T)) - option.r * K * np.exp(-option.r * option.T) * norm.cdf(d2)
+
+            # Rho
+            rho = K * option.T * np.exp(-option.r * option.T) * norm.cdf(d2)
+
+            return delta, gamma, vega, theta, rho
+
+        underlying_strikes = np.linspace(K_min, K_max, 100)
+
+        greeks_values = np.array([greeks_K(strike) for strike in underlying_strikes])
+
+        # Plot des grecques
+        plt.figure(figsize=(12, 8))
+
+        labels = ['Delta', 'Gamma', 'Vega', 'Theta', 'Rho']
+
+        for i in range(5):
+            plt.subplot(2, 3, i + 1)
+            plt.plot(underlying_strikes, greeks_values[:, i], label=labels[i])
+            plt.title(labels[i])
+            plt.xlabel('Strike')
             plt.legend()
 
         plt.tight_layout()
